a98d156b8c35c91338a160f3e9a0ea77febc9e79
unai <unaiduran.upv@gmail.com>
2023-05-01 15:44:44 +0200

    - Lista de recetas

---
+++ app/src/main/java/com/example/proyectofinal/AdaptadorListaRecetas.java
@@ -0,0 +1,63 @@
+package com.example.proyectofinal;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class AdaptadorListaRecetas extends RecyclerView.Adapter<ListaRecetasViewHolder> {
+
+    Context context;
+    //List<Recipe> list;
+    String listaTit[] = {"macarrones", "lenetejas"};
+
+    public AdaptadorListaRecetas(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ListaRecetasViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ListaRecetasViewHolder(LayoutInflater.from(context).inflate(R.layout.lista_recetas, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListaRecetasViewHolder holder, int position) {
+        holder.titulo.setText(listaTit[position]);
+        holder.titulo.setSelected(true);
+        holder.num_likes.setText("20 likes");
+        holder.tiempo_preparacion.setText("10 minutos");
+       /* holder.titulo.setText(list.get(position).title);
+        holder.titulo.setSelected(true);
+        holder.num_likes.setSelected(list.get(position).aggregateLikes+ " Likes");
+        holder.tiempo_preparacion.setText(list.get(position).time+ " Minutos");
+        Picasso.get().load(list.get(position).image.into(holder.imagen_comida))*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;//list.size()
+    }
+}
+class ListaRecetasViewHolder extends RecyclerView.ViewHolder{
+    CardView recetas_lay;
+    TextView titulo, tiempo_preparacion, num_likes;
+    ImageView imagen_comida;
+
+    public ListaRecetasViewHolder(@NonNull View itemView){
+        super(itemView);
+        recetas_lay = itemView.findViewById(R.id.recetas_lay);
+        titulo = itemView.findViewById(R.id.titulo);
+        tiempo_preparacion = itemView.findViewById(R.id.tiempo_preparacion);
+        num_likes = itemView.findViewById(R.id.num_likes);
+        imagen_comida = itemView.findViewById(R.id.imagen_comida);
+    }
+}
--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -5,6 +5,9 @@ import static androidx.navigation.fragment.FragmentKt.findNavController;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.Presentation;
+import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
 import androidx.browser.customtabs.CustomTabsIntent;
@@ -12,18 +15,22 @@ import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     //private String urlChat = "https://www.google.com";
-
-
+    AdaptadorListaRecetas adaptadorListaRecetas;
+    RecyclerView recyclerView;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -31,12 +38,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = findViewById(R.id.recycler_lista);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 1));
+        adaptadorListaRecetas = new AdaptadorListaRecetas(MainActivity.this); //Aqui se le pasaria una lista con las recetas tambien
+        recyclerView.setAdapter(adaptadorListaRecetas);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
         NavController navController = Navigation.findNavController(this, R.id.fragmentContainerView);
 
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
-
-
 
     }
 
