eac5199102fce0fdfacb664d09a2a89731d40f7c
korah91 <62379054+korah91@users.noreply.github.com>
2023-05-01 12:29:28 +0200

    añadido el constraint layout

--- app/src/main/java/com/example/proyectofinal/AnadirReceta.java
+++ app/src/main/java/com/example/proyectofinal/AnadirReceta.java
@@ -59,6 +59,11 @@ public class AnadirReceta extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
+
+
         return inflater.inflate(R.layout.fragment_anadir_receta, container, false);
     }
 }
