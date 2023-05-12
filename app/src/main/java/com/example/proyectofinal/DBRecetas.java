b53881364faff26729e1ec7330cd3bd6ab14734a
Estefania <estefaniaonatell01@gmail.com>
2023-05-12 17:49:10 +0200

    Recetas del usuario sin interfaz

--- app/src/main/java/com/example/proyectofinal/DBRecetas.java
+++ app/src/main/java/com/example/proyectofinal/DBRecetas.java
@@ -24,6 +24,7 @@ public class DBRecetas {
 
     Context context;
     final ArrayList<Receta> listaRecetas;
+    final ArrayList<Receta> listaRec;
     boolean booleanDevolver = false;
     boolean primera = true;
 
@@ -33,6 +34,7 @@ public class DBRecetas {
         this.context = context;
         // Se crea el arrayList a devolver
         this.listaRecetas = new ArrayList<Receta>();
+        this.listaRec = new ArrayList<Receta>();
     }
 
     public ArrayList<Receta> getRecetasGlobales(boolean vegetariano, boolean vegano, boolean sinGluten) {
@@ -113,6 +115,93 @@ public class DBRecetas {
                         } else {
                             Log.d("getRecetasGlobales", "No se han podido conseguir las Recetas Globales");
                             Log.d("getRecetasGlobales", response.toString());
+                        }
+                        response.close();
+                    } catch (JSONException e) {
+                        Log.d("viewholder", "Ha habido excepcion" + e.toString());
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+        }
+        return getListaRecetas();
+    }
+
+    public ArrayList<Receta> getRecetasUsuario(String usuario) {
+
+        // Utilizo la libreria OkHttp
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+
+        // Crear un objeto JSON con los campos correspondientes
+        JSONObject json = new JSONObject();
+        try {
+            json.put("usuario", usuario);
+        } catch (JSONException e) {
+            Log.d("getRecetasUsuario", "excepcion");
+            throw new RuntimeException(e);
+        }
+
+        RequestBody body = RequestBody.create(mediaType, json.toString());
+
+        Log.d("getRecetasUsuario", json.toString());
+
+        Request request = new Request.Builder()
+                .url("http://ec2-54-93-62-124.eu-central-1.compute.amazonaws.com/eonate006/WEB/Proyecto_final/select_Recetas_Usuario.php")
+                .post(body)
+                .build();
+        // Mientras que no se descarguen las recetas no devuelve
+        while (!booleanDevolver){
+            //booleanDevolver = true;
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    // Error de conexión o I/O
+                    Log.d("getRecetasUsuario", "No se han podido conseguir las Recetas");
+                    Log.d("getRecetasUsuario", e.toString());
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        if (response.isSuccessful()) {
+                            // Solicitud exitosa
+                            // Se crea el array JSON con TODAS las recetas
+                            String datos = response.body().string();
+
+                            JSONArray jsonArray = new JSONArray(datos);
+                            // Se itera sobre el JSON
+                            if(primera) {
+                                primera = false;
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                    // Se obtiene la informacion de la receta para luego guardarla en ArrayList
+                                    int idReceta = jsonObject.getInt("idReceta");
+                                    String nombre = jsonObject.getString("nombre");
+                                    String urlFoto = jsonObject.getString("urlFoto");
+
+                                    // Si son == 1 creamos la receta con esos valores como true, si no pues false
+                                    boolean vegetariano = jsonObject.getInt("vegetariano") == 1;
+                                    boolean vegano = jsonObject.getInt("vegano") == 1;
+                                    boolean sinGluten = jsonObject.getInt("sinGluten") == 1;
+
+                                    String ingredientes = jsonObject.getString("ingredientes");
+                                    String instrucciones = jsonObject.getString("instrucciones");
+                                    int tiempo = jsonObject.getInt("tiempo");
+                                    String usuario = jsonObject.getString("usuario");
+
+                                    // Se crea el objeto Receta
+                                    Receta r = new Receta(idReceta, usuario, nombre, tiempo, ingredientes, instrucciones, vegetariano, vegano, sinGluten, urlFoto);
+                                    // Se guarda en el arrayList
+                                    addToListaRecetas(r);
+                                }
+                                booleanDevolver = true;
+                            }
+
+                        } else {
+                            Log.d("getRecetasUsuario", "No se han podido conseguir las Recetas Globales");
+                            Log.d("getRecetasUsuario", response.toString());
                         }
                         response.close();
                     } catch (JSONException e) {
--- app/src/main/java/com/example/proyectofinal/RecetasUsuario.java
+++ app/src/main/java/com/example/proyectofinal/RecetasUsuario.java
@@ -2,13 +2,41 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class RecetasUsuario extends AppCompatActivity {
-
+    Context context;
+    ArrayList<Receta> listaRec;
+    boolean booleanDevolver = false;
+    boolean primera = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recetas_usuario);
+        Intent extras = getIntent();
+        String usuario = extras.getStringExtra("nombreUsuario");
+        DBRecetas dbRecetas = new DBRecetas(RecetasUsuario.this);
+        listaRec = dbRecetas.getRecetasUsuario(usuario);
+        Log.d("getRecetasUsuario", "listaRecetasLongitud " +listaRec.size());
+
     }
+
 }
